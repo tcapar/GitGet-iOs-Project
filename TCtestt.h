eaaa47b6340aa66fb7eb009f6377fa737d241e38
Tarik Capar <Tarik@Tariks-iMac.local>
2014-08-28 21:41:14 +0200

    slaganja po uputstvima

---
+++ TCtestt.h
@@ -0,0 +1,13 @@
+//
+//  TCtestt.h
+//  GitGet
+//
+//  Created by Tarik Capar on 28/08/14.
+//  Copyright (c) 2014 Tarik Capar. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TCtestt : NSObject
+
+@end
---
+++ TCtestt.m
@@ -0,0 +1,13 @@
+//
+//  TCtestt.m
+//  GitGet
+//
+//  Created by Tarik Capar on 28/08/14.
+//  Copyright (c) 2014 Tarik Capar. All rights reserved.
+//
+
+#import "TCtestt.h"
+
+@implementation TCtestt
+
+@end
